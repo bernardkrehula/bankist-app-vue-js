d561b68a14144b65eb9fee3fa310942c95020f93
Bernard Krehula <krehula.bernard456@gmail.com>
2026-08-18 23:30:59 +0200

    feat: add <Input> component

---
+++ src/components/ui/input.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "input",
+  data() {
+    return {
+      user: "Initial value",
+      pin: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <input type="text" placeholder="user" v-model="user" />
+</template>
